3d2456ed7f24a9ab4f807e9f6582f31fbe9e3d83
Annabelle <annabelle.braye@gmail.com>
2024-04-26 19:03:29 +0200

    feat(router): Add router, pages and routes to pages, remove tailwind.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/index.scss'
+import Home from './pages/Home'
+import About from './pages/About'
+import Rental from './pages/Rental'
+import Error from './pages/Error'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/rental/:id' element={<Rental />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+    </Router>
+    <Home />
   </React.StrictMode>,
 )
