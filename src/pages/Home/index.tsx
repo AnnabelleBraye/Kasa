9d9320168e1b4da0a800ababe3618ae5ffe3446e
Annabelle <annabelle.braye@gmail.com>
2024-04-29 17:40:08 +0200

    feat(home): Add container with background and h1 title for mobile and desktop.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container">
+      <div className="title-container">
+        <h1 className="main-title main-title__mobile">Chez vous,< br /> partout et ailleurs</h1>
+        <h1 className="main-title main-title__desktop">Chez vous, partout et ailleurs</h1>
+      </div>
+    </div>
   )
 }
 
