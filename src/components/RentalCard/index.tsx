455d826efe7775e2dcd8980b3b65ddad66342686
Annabelle <annabelle.braye@gmail.com>
2024-04-29 19:01:39 +0200

    feat(home): Creation of the rental component, add list of rental, style on mobile and desktop.

---
+++ src/components/RentalCard/index.tsx
@@ -0,0 +1,11 @@
+const RentalCard = () => {
+  return (
+    <article className="rental-card">
+      <img src="./src/assets/images/about_image_full.png" alt="Rental picture" />
+
+      <h2 className="rental-title">Titre de la location</h2>
+    </article>
+  )
+}
+
+export default RentalCard
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import RentalCard from "../../components/RentalCard";
+
 const Home = () => {
   return (
     <div className="container">
@@ -5,6 +7,14 @@ const Home = () => {
         <h1 className="main-title main-title__mobile">Chez vous,< br /> partout et ailleurs</h1>
         <h1 className="main-title main-title__desktop">Chez vous, partout et ailleurs</h1>
       </div>
+      <section className="rental-container">
+        <RentalCard />
+        <RentalCard />
+        <RentalCard />
+        <RentalCard />
+        <RentalCard />
+        <RentalCard />
+      </section>
     </div>
   )
 }
