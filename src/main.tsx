4edbbf8a97dbba8d74384e3711ca3e0b2a84dee8
Annabelle <annabelle.braye@gmail.com>
2024-05-01 17:48:06 +0200

    feat(rental): Add FontAwesome for React, create rental page with css for mobile and desktop and all components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import './styles/main.scss'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import Home from './pages/Home'
 import About from './pages/About'
 import Rental from './pages/Rental'
 import Error from './pages/Error'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
