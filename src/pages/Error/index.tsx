ae87ea4373442ec41fee633d34cdc06158acdf00
Annabelle <annabelle.braye@gmail.com>
2024-05-02 18:16:15 +0200

    feat(error): Creation of error page with mobile and desktop css.

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="error-container">
+      <h1>404</h1>
+      <p>Oups! La page que <br className="break-text" />
+      vous demandez n'existe pas.</p>
+      <Link className='error-container__link' to='/'>Retourner sur la page d’accueil</Link>
+    </div>
   )
 }
 
